0be3afdaa44fb677a194009d848b424b1b985803
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2023-12-05 11:55:04 +0600

    update

--- spring-boot-simple-web/src/main/java/com/imranmadbar/controller/HomeController.java
+++ spring-boot-simple-web/src/main/java/com/imranmadbar/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
 
 	@GetMapping(value = "/")
 	public String welcomeMsg() {
-		System.out.print("RestUrl: http://localhost:8080/api/1");
+		System.out.println("ForRestUrl: http://localhost:8080/api/1");
 		logger.info("Welcome to SimpleWebApplication");
 		return "Welcome to SimpleWebApplication";
 	}
