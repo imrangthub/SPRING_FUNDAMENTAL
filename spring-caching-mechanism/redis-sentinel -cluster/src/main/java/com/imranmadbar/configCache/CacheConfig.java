ad67d9f3eb2714b116ccb82ad62c627746068431
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2023-12-12 16:32:48 +0600

    upd

--- spring-caching-mechanism/redis-sentinel -cluster/src/main/java/com/imranmadbar/configCache/CacheConfig.java
+++ spring-caching-mechanism/redis-sentinel -cluster/src/main/java/com/imranmadbar/configCache/CacheConfig.java
@@ -17,6 +17,7 @@ import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -54,11 +55,14 @@ public class CacheConfig {
 		return new LettuceConnectionFactory(sentinelConfig);
 	}
 
-	@Bean
-	public RedisCacheConfiguration cacheConfiguration() {
-		return RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(600));
-	}
 
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+    
 
 	@Bean
 	public RedisCacheManager cacheManager() {
